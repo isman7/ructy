3185289b1d6299d80a8acbe4051511c16dc36a28
Ismael Benito <isman7@gmail.com>
2018-01-19 21:11:14 +0100

    Parsing universities pages.

--- ructy/main.py
+++ ructy/main.py
@@ -1,10 +1,46 @@
 import requests
 import pandas
+from bs4 import BeautifulSoup
 
-response = requests.get("https://www.educacion.gob.es/ruct/listauniversidades?tipo_univ=&d-8320336-p=6&cccaa=&actual=universidades&consulta=1&codigoUniversidad=",
-             verify=False)
+BASE_URL = "https://www.educacion.gob.es/{}"
 
-dataframe = pandas.read_html(response.content.decode(encoding='utf-8', errors="ignore"),
-                             attrs={"id": "universidad"})
+QUERY_TMP = """
+ruct/
+listauniversidades?
+tipo_univ=&
+d-8320336-p={}&
+cccaa=&
+actual=universidades&
+consulta=1&
+codigoUniversidad=
+"""
 
-print(dataframe)
+
+def get_url(query):
+    return BASE_URL.format(query)
+
+
+def get_universities_page_url(page):
+    return get_url(QUERY_TMP.replace("\n", "").format(page))
+
+
+d = []
+
+for n in range(9):
+    i = n + 1
+    response = requests.get(get_universities_page_url(i), verify=False)
+
+    html = BeautifulSoup(response.content, "lxml")
+    table = html.find("table", {"id": "universidad"})
+
+    headers = [header.contents[0] for header in table.findAll("th")]
+
+    universities = [uni.findAll("td") for uni in table.find("tbody").findAll("tr")]
+
+    d = d + [{headers[ii]: col for ii, col in enumerate(uni)} for uni in universities]
+
+for col in d:
+    a = col["Universidad"].find("a")
+    print(a.contents[0])
+    print(a['href'])
+
